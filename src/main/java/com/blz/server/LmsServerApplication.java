f5d84997d54db29211b50bc510eef3fa2c40518e
PavanKumar107 <gvpavangv14@gmail.com>
2022-09-05 17:42:10 +0530

    server

---
+++ src/main/java/com/blz/server/LmsServerApplication.java
@@ -0,0 +1,15 @@
+package com.blz.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blz/server/LmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blz.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
